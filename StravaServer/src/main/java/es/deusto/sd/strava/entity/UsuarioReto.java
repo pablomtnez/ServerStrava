0226b61f09bb0ddee188e417852c841e0b19c009
pablomtnez <91073313+pablomtnez@users.noreply.github.com>
2024-11-14 11:21:18 +0100

    Comentarios IA

--- StravaServer/src/main/java/es/deusto/sd/strava/entity/UsuarioReto.java
+++ StravaServer/src/main/java/es/deusto/sd/strava/entity/UsuarioReto.java
@@ -1,3 +1,9 @@
+/**
+ * This code is based on solutions provided by ChatGPT 4o and 
+ * adapted using GitHub Copilot. It has been thoroughly reviewed 
+ * and validated to ensure correctness and that it is free of errors.
+ */
+
 package es.deusto.sd.strava.entity;
 
 public class UsuarioReto {
